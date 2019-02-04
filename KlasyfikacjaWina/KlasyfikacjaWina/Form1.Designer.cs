e50a5a09da8832e8657ced5eae477519ce768c63
amaciej <amaciejewski123@gmail.com>
2019-02-04 01:35:15 +0100

    add

---
+++ KlasyfikacjaWina/KlasyfikacjaWina/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace KlasyfikacjaWina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAlcohol = new System.Windows.Forms.TextBox();
+            this.txtMalic = new System.Windows.Forms.TextBox();
+            this.txtAsh = new System.Windows.Forms.TextBox();
+            this.txtAlcalinity = new System.Windows.Forms.TextBox();
+            this.txtMagnesium = new System.Windows.Forms.TextBox();
+            this.txtPhenols = new System.Windows.Forms.TextBox();
+            this.txtFlavanoids = new System.Windows.Forms.TextBox();
+            this.txtNonflavanoid = new System.Windows.Forms.TextBox();
+            this.txtProanth = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtHue = new System.Windows.Forms.TextBox();
+            this.txtOD = new System.Windows.Forms.TextBox();
+            this.txtProline = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.butKlasa = new System.Windows.Forms.Button();
+            this.lblKlasa = new System.Windows.Forms.Label();
+            this.butClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAlcohol
+            // 
+            this.txtAlcohol.Location = new System.Drawing.Point(184, 19);
+            this.txtAlcohol.Name = "txtAlcohol";
+            this.txtAlcohol.ReadOnly = true;
+            this.txtAlcohol.Size = new System.Drawing.Size(100, 20);
+            this.txtAlcohol.TabIndex = 0;
+            // 
+            // txtMalic
+            // 
+            this.txtMalic.Location = new System.Drawing.Point(184, 50);
+            this.txtMalic.Name = "txtMalic";
+            this.txtMalic.ReadOnly = true;
+            this.txtMalic.Size = new System.Drawing.Size(100, 20);
+            this.txtMalic.TabIndex = 1;
+            // 
+            // txtAsh
+            // 
+            this.txtAsh.Location = new System.Drawing.Point(184, 83);
+            this.txtAsh.Name = "txtAsh";
+            this.txtAsh.ReadOnly = true;
+            this.txtAsh.Size = new System.Drawing.Size(100, 20);
+            this.txtAsh.TabIndex = 2;
+            // 
+            // txtAlcalinity
+            // 
+            this.txtAlcalinity.Location = new System.Drawing.Point(184, 113);
+            this.txtAlcalinity.Name = "txtAlcalinity";
+            this.txtAlcalinity.ReadOnly = true;
+            this.txtAlcalinity.Size = new System.Drawing.Size(100, 20);
+            this.txtAlcalinity.TabIndex = 3;
+            // 
+            // txtMagnesium
+            // 
+            this.txtMagnesium.Location = new System.Drawing.Point(184, 143);
+            this.txtMagnesium.Name = "txtMagnesium";
+            this.txtMagnesium.ReadOnly = true;
+            this.txtMagnesium.Size = new System.Drawing.Size(100, 20);
+            this.txtMagnesium.TabIndex = 4;
+            // 
+            // txtPhenols
+            // 
+            this.txtPhenols.Location = new System.Drawing.Point(184, 172);
+            this.txtPhenols.Name = "txtPhenols";
+            this.txtPhenols.ReadOnly = true;
+            this.txtPhenols.Size = new System.Drawing.Size(100, 20);
+            this.txtPhenols.TabIndex = 5;
+            // 
+            // txtFlavanoids
+            // 
+            this.txtFlavanoids.Location = new System.Drawing.Point(184, 202);
+            this.txtFlavanoids.Name = "txtFlavanoids";
+            this.txtFlavanoids.ReadOnly = true;
+            this.txtFlavanoids.Size = new System.Drawing.Size(100, 20);
+            this.txtFlavanoids.TabIndex = 6;
+            // 
+            // txtNonflavanoid
+            // 
+            this.txtNonflavanoid.Location = new System.Drawing.Point(184, 232);
+            this.txtNonflavanoid.Name = "txtNonflavanoid";
+            this.txtNonflavanoid.ReadOnly = true;
+            this.txtNonflavanoid.Size = new System.Drawing.Size(100, 20);
+            this.txtNonflavanoid.TabIndex = 7;
+            // 
+            // txtProanth
+            // 
+            this.txtProanth.Location = new System.Drawing.Point(184, 262);
+            this.txtProanth.Name = "txtProanth";
+            this.txtProanth.ReadOnly = true;
+            this.txtProanth.Size = new System.Drawing.Size(100, 20);
+            this.txtProanth.TabIndex = 8;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(184, 290);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.ReadOnly = true;
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 9;
+            // 
+            // txtHue
+            // 
+            this.txtHue.Location = new System.Drawing.Point(184, 319);
+            this.txtHue.Name = "txtHue";
+            this.txtHue.ReadOnly = true;
+            this.txtHue.Size = new System.Drawing.Size(100, 20);
+            this.txtHue.TabIndex = 10;
+            // 
+            // txtOD
+            // 
+            this.txtOD.Location = new System.Drawing.Point(184, 347);
+            this.txtOD.Name = "txtOD";
+            this.txtOD.ReadOnly = true;
+            this.txtOD.Size = new System.Drawing.Size(100, 20);
+            this.txtOD.TabIndex = 11;
+            // 
+            // txtProline
+            // 
+            this.txtProline.Location = new System.Drawing.Point(184, 376);
+            this.txtProline.Name = "txtProline";
+            this.txtProline.ReadOnly = true;
+            this.txtProline.Size = new System.Drawing.Size(100, 20);
+            this.txtProline.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(316, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 54);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Uzupełnij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Alcohol";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Malic acid";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Ash";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Alcalinity of ash ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Magnesium";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Total phenols";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Flavanoids";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Nonflavanoid phenols";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(34, 262);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Proanthocyanins";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(45, 293);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Color intensity";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(60, 322);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Hue";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(8, 350);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(156, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "OD280/OD315 of diluted wines";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(45, 379);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Proline ";
+            // 
+            // butKlasa
+            // 
+            this.butKlasa.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.butKlasa.Location = new System.Drawing.Point(464, 24);
+            this.butKlasa.Name = "butKlasa";
+            this.butKlasa.Size = new System.Drawing.Size(291, 31);
+            this.butKlasa.TabIndex = 27;
+            this.butKlasa.Text = "START";
+            this.butKlasa.UseVisualStyleBackColor = true;
+            this.butKlasa.Click += new System.EventHandler(this.butKlasa_Click);
+            // 
+            // lblKlasa
+            // 
+            this.lblKlasa.AutoSize = true;
+            this.lblKlasa.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblKlasa.Location = new System.Drawing.Point(552, 96);
+            this.lblKlasa.Name = "lblKlasa";
+            this.lblKlasa.Size = new System.Drawing.Size(0, 24);
+            this.lblKlasa.TabIndex = 28;
+            // 
+            // butClear
+            // 
+            this.butClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.butClear.Location = new System.Drawing.Point(316, 83);
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(86, 37);
+            this.butClear.TabIndex = 29;
+            this.butClear.Text = "Wyczyść";
+            this.butClear.UseVisualStyleBackColor = true;
+            this.butClear.Click += new System.EventHandler(this.butClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butClear);
+            this.Controls.Add(this.lblKlasa);
+            this.Controls.Add(this.butKlasa);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtProline);
+            this.Controls.Add(this.txtOD);
+            this.Controls.Add(this.txtHue);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtProanth);
+            this.Controls.Add(this.txtNonflavanoid);
+            this.Controls.Add(this.txtFlavanoids);
+            this.Controls.Add(this.txtPhenols);
+            this.Controls.Add(this.txtMagnesium);
+            this.Controls.Add(this.txtAlcalinity);
+            this.Controls.Add(this.txtAsh);
+            this.Controls.Add(this.txtMalic);
+            this.Controls.Add(this.txtAlcohol);
+            this.Name = "Form1";
+            this.Text = "SSN Klasyfikacja Wina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAlcohol;
+        private System.Windows.Forms.TextBox txtMalic;
+        private System.Windows.Forms.TextBox txtAsh;
+        private System.Windows.Forms.TextBox txtAlcalinity;
+        private System.Windows.Forms.TextBox txtMagnesium;
+        private System.Windows.Forms.TextBox txtPhenols;
+        private System.Windows.Forms.TextBox txtFlavanoids;
+        private System.Windows.Forms.TextBox txtNonflavanoid;
+        private System.Windows.Forms.TextBox txtProanth;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtHue;
+        private System.Windows.Forms.TextBox txtOD;
+        private System.Windows.Forms.TextBox txtProline;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button butKlasa;
+        private System.Windows.Forms.Label lblKlasa;
+        private System.Windows.Forms.Button butClear;
+    }
+}
+
---
+++ KlasyfikacjaWina/KlasyfikacjaWina/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlasyfikacjaWina
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //                 Wczytanie z pliku loswej linii danych 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            //string[] data = new string[14];
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"C:\Users\Adam\Documents\sem. 5\Zastosowanie Sztucznej inteligencji\KlasyfikacjaWina\KlasyfikacjaWina\dane.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] dane = line.Split(',');
+
+                txtAlcohol.Text = dane[1];
+                txtMalic.Text = dane[2];
+                txtAsh.Text = dane[3];
+                txtAlcalinity.Text = dane[4];
+                txtMagnesium.Text = dane[5];
+                txtPhenols.Text = dane[6];
+                txtFlavanoids.Text = dane[7];
+                txtNonflavanoid.Text = dane[8];
+                txtProanth.Text = dane[9];
+                txtColor.Text = dane[10];
+                txtHue.Text = dane[11];
+                txtOD.Text = dane[12];
+                txtProline.Text = dane[13];
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+
+        private void butClear_Click(object sender, EventArgs e)
+        {
+            txtProline.Text = txtProanth.Text = txtPhenols.Text = txtOD.Text = 
+                txtNonflavanoid.Text = txtMalic.Text = txtMagnesium.Text = txtHue.Text = 
+                txtFlavanoids.Text = txtColor.Text = txtAsh.Text = txtAlcohol.Text = txtAlcalinity.Text = "";
+        }
+
+        private void butKlasa_Click(object sender, EventArgs e)
+        {
+            PropagacjaWPrzód();
+        }
+
+
+
+        public void PropagacjaWPrzód()
+        {
+            int U0 = 0;
+            double U1 = double.Parse(txtAlcohol.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U2 = double.Parse(txtMalic.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U3 = double.Parse(txtAsh.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U4 = double.Parse(txtAlcalinity.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U5 = double.Parse(txtMagnesium.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U6 = double.Parse(txtPhenols.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U7 = double.Parse(txtFlavanoids.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U8 = double.Parse(txtNonflavanoid.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U9 = double.Parse(txtProanth.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U10 = double.Parse(txtColor.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U11 = double.Parse(txtHue.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U12 = double.Parse(txtOD.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double U13 = double.Parse(txtProline.Text, CultureInfo.InvariantCulture.NumberFormat);
+
+            double[] wagi = new double[112];
+
+            Random rnd = new Random();
+            //   Wagi początkowe
+            for(int i = 0; i < wagi.Length; i++)
+            {
+                double waga = rnd.Next(1, 5);
+                wagi[i] = waga;
+                
+            }
+
+            for (int i = 0; i < wagi.Length; i++)
+            {
+                Console.WriteLine("tablica[{0}] = {1}", i, wagi[i]);
+            }
+
+            //    Warstwa ukryta 
+            double S1 = U1 * wagi[0] + U2 * wagi[1] + U3 * wagi[2] + U4 * wagi[3] + U5 * wagi[4]
+                        + U6 * wagi[5] + U7 * wagi[6] + U8 * wagi[7] + U9 * wagi[8] + U10 * wagi[9]
+                        + U11 * wagi[10] + U12 * wagi[11] + U13 * wagi[12];
+            double U14 = 1 / (1 + Math.Exp(-S1));
+            Console.WriteLine("S1 = " + S1);
+            Console.WriteLine("U14 = " + U14);
+            double S2 = U1 * wagi[13] + U2 * wagi[14] + U3 * wagi[15] + U4 * wagi[16] + U5 * wagi[17]
+                        + U6 * wagi[18] + U7 * wagi[19] + U8 * wagi[20] + U9 * wagi[21] + U10 * wagi[22]
+                        + U11 * wagi[23] + U12 * wagi[24] + U13 * wagi[25];
+            double U15 = 1 / (1 + Math.Exp(-S2));
+            Console.WriteLine("S2 = " + S2);
+            Console.WriteLine("U15 = " + U15);
+            double S3 = U1 * wagi[26] + U2 * wagi[27] + U3 * wagi[28] + U4 * wagi[29] + U5 * wagi[30]
+                        + U6 * wagi[31] + U7 * wagi[32] + U8 * wagi[33] + U9 * wagi[34] + U10 * wagi[35]
+                        + U11 * wagi[36] + U12 * wagi[37] + U13 * wagi[38];
+            double U16 = 1 / (1 + Math.Exp(-S3));
+            Console.WriteLine("S3 = " + S3);
+            Console.WriteLine("U16 = " + U16);
+            double S4 = U1 * wagi[39] + U2 * wagi[40] + U3 * wagi[41] + U4 * wagi[42] + U5 * wagi[43]
+                        + U6 * wagi[44] + U7 * wagi[45] + U8 * wagi[46] + U9 * wagi[47] + U10 * wagi[48]
+                        + U11 * wagi[49] + U12 * wagi[50] + U13 * wagi[51];
+            double U17 = 1 / (1 + Math.Exp(-S4));
+            Console.WriteLine("S4 = " + S4);
+            Console.WriteLine("U17 = " + U17);
+            double S5 = U1 * wagi[52] + U2 * wagi[53] + U3 * wagi[54] + U4 * wagi[55] + U5 * wagi[56]
+                        + U6 * wagi[57] + U7 * wagi[58] + U8 * wagi[59] + U9 * wagi[60] + U10 * wagi[61]
+                        + U11 * wagi[62] + U12 * wagi[63] + U13 * wagi[64];
+            double U18 = 1 / (1 + Math.Exp(-S5));
+            Console.WriteLine("S5 = " + S5);
+            Console.WriteLine("U18 = " + U18);
+            double S6 = U1 * wagi[65] + U2 * wagi[66] + U3 * wagi[67] + U4 * wagi[68] + U5 * wagi[69]
+                        + U6 * wagi[70] + U7 * wagi[71] + U8 * wagi[72] + U9 * wagi[73] + U10 * wagi[74]
+                        + U11 * wagi[75] + U12 * wagi[76] + U13 * wagi[77];
+            double U19 = 1 / (1 + Math.Exp(-S6));
+            Console.WriteLine("S6 = " + S6);
+            Console.WriteLine("U19 = " + U19);
+            double S7 = U1 * wagi[78] + U2 * wagi[79] + U3 * wagi[80] + U4 * wagi[81] + U5 * wagi[82]
+                        + U6 * wagi[83] + U7 * wagi[84] + U8 * wagi[85] + U9 * wagi[86] + U10 * wagi[87]
+                        + U11 * wagi[88] + U12 * wagi[89] + U13 * wagi[90];
+            double U20 = 1 / (1 + Math.Exp(-S7));
+            Console.WriteLine("S7 = " + S7);
+            Console.WriteLine("U20 = " + U20);
+
+            //        Warstwa Wyjściowa
+            double S8 = U14 * wagi[91] + U15 * wagi[92] + U16 * wagi[93] + U17 * wagi[94] + U18 * wagi[95]
+                + U19 * wagi[96] + U20 * wagi[97];
+            double U21 = 1 / (1 + Math.Exp(-S8));
+            Console.WriteLine("S8 = " + S8);
+            Console.WriteLine("U21 = " + U21);
+            double S9 = U14 * wagi[98] + U15 * wagi[99] + U16 * wagi[100] + U17 * wagi[101] + U18 * wagi[102]
+                + U19 * wagi[103] + U20 * wagi[104];
+            double U22 = 1 / (1 + Math.Exp(-S9));
+            Console.WriteLine("S9 = " + S9);
+            Console.WriteLine("U22 = " + U22);
+            double S10 = U14 * wagi[105] + U15 * wagi[106] + U16 * wagi[107] + U17 * wagi[108] + U18 * wagi[109]
+                + U19 * wagi[110] + U20 * wagi[111];
+            double U23 = 1 / (1 + Math.Exp(-S10));
+            Console.WriteLine("S10 = " + S10);
+            Console.WriteLine("U23 = " + U23);
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
